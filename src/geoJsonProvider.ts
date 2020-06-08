4b935492bf69f7e9ad7d2edd7e6122c39e7682c7
Jimmy Denis <jimmy.denis@datakhi.com>
2020-06-08 16:46:36 +0200

    ajout de l'arrondissement (pour le haut de france)

--- src/geoJsonProvider.ts
+++ src/geoJsonProvider.ts
@@ -1,5 +1,6 @@
 import departements from "../assets/geoJson/departements.json";
 import regions from "../assets/geoJson/regions.json";
+import arrHDF from "../assets/geoJson/arrondissements-hauts-de-france.json";
 
 export function getJson(name:string){
     var result;
@@ -9,6 +10,9 @@ export function getJson(name:string){
             break;
         case 'regions' :
             result = regions;
+            break;
+        case 'arrondissements' :
+            result = arrHDF;
             break;
     }
     return result;
